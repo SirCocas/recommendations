7f39f0623bd897568a8d12b5569e629bacc263cb
SirCocas <sofiateixeiravaz@ua.pt>
2026-01-06 12:37:24 +0000

    asd

--- execute.sh
+++ execute.sh
@@ -1,5 +1,4 @@
-sudo apt-get update && \
-  DEBIAN_FRONTEND=noninteractive apt-get install -y python3-pip python3-venv && \
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3-pip python3-venv && \
   pip3 install beautifulsoup4
 
 python3 -m venv venv && source venv/bin/activate && pip install beautifulsoup4 && python3 novo.py
