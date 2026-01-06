8bb62bfca263f731bdc87746e0fc86addef70970
SirCocas <sofiateixeiravaz@ua.pt>
2026-01-06 12:48:38 +0000

    requests

--- execute.sh
+++ execute.sh
@@ -1,4 +1,6 @@
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3-pip python3-venv && \
-  pip3 install beautifulsoup4 requests
+  pip3 install beautifulsoup4 
+
+pip3 install --user requests
 
 python3 -m venv venv && source venv/bin/activate && pip install beautifulsoup4 && python3 novo.py
