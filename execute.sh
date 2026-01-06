d79824c2be2b805399e18cc4c4113cda5fd70765
SirCocas <sofiateixeiravaz@ua.pt>
2026-01-06 15:22:10 +0000

    sleep

--- execute.sh
+++ execute.sh
@@ -1,6 +1,1 @@
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3-pip python3-venv && \
-  pip3 install beautifulsoup4 
-
-curl https://www.anime-planet.com/anime/chainsaw-man-the-movie-reze-arc/recommendations
-
-python3 -m venv venv && source venv/bin/activate && pip install beautifulsoup4 && pip install requests && python3 novo.py
+sleep 10
